bbfbe59b90c6e0386d61b82120026d7a9f3598f9
AndreasGrenz <a.grenz@outlook.de>
2026-02-21 10:15:50 +0100

    Initial commit: React cource

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+import Homework_06 from "./homeworks/homework_06/homework_06";
+import Lesson_07 from "./lessons/Lesson_07/Lesson_07"
+
+function App() {
+  return (
+  <>
+  {/*Homeworks */}
+  {/* <Homework_06 /> */}
+
+  {/*Lessons */}
+  {/* <Lesson_06/> */}
+  <Lesson_07 />
+  </>
+);
+}
+export default App;
